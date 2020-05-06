1dd3e4546f10a84e19fd44b8198831d5ac64b98b
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-05-06 11:42:22 +0200

    Download replacement image file directly.

--- camels/__init__.py
+++ camels/__init__.py
@@ -1,58 +1,64 @@
 from camels.data import gauge_information, gauge_ids
-from urllib import request
-from tempfile import NamedTemporaryFile
-import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 def plot_overview():
     """
     Plots overview over geographic locations of gauges available in
     the dataset.
     """
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from matplotlib.gridspec import GridSpec
-    from cartopy.io.img_tiles import Stamen
-    from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
-    import cartopy.crs as ccrs
+    try:
+        from cartopy.io.img_tiles import Stamen
+        from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+        import cartopy.crs as ccrs
 
-    tiler = Stamen("terrain")
-    mercator = tiler.crs
+        tiler = Stamen("terrain")
+        mercator = tiler.crs
 
-    f = plt.figure(figsize = (8, 6))
-    gs = GridSpec(1, 2, width_ratios=[1.0, 0.03])
+        f = plt.figure(figsize = (10, 8))
+        gs = GridSpec(1, 2, width_ratios=[1.0, 0.03])
 
-    ax = plt.subplot(gs[0], projection=mercator)
-    ax.set_extent([-130, -60, 20, 50])
-    ax.add_image(tiler, 5)
-    #ax.coastlines('10m', lw=0.5)
+        ax = plt.subplot(gs[0], projection=mercator)
+        ax.set_extent([-130, -60, 20, 50])
+        ax.add_image(tiler, 5)
+        #ax.coastlines('10m', lw=0.5)
 
-    ax.set_xlabel("Longitude")
-    ax.set_ylabel("Latitude")
-    xticks = np.arange(-135, -64, 10)
-    yticks = np.arange(25, 46, 10)
-    ax.set_xticks(xticks, crs=ccrs.PlateCarree())
-    ax.set_yticks(yticks, crs=ccrs.PlateCarree())
-    lon_formatter = LongitudeFormatter(zero_direction_label=True)
-    lat_formatter = LatitudeFormatter()
-    ax.xaxis.set_major_formatter(lon_formatter)
-    ax.yaxis.set_major_formatter(lat_formatter)
+        ax.set_xlabel("Longitude")
+        ax.set_ylabel("Latitude")
+        xticks = np.arange(-135, -64, 10)
+        yticks = np.arange(25, 46, 10)
+        ax.set_xticks(xticks, crs=ccrs.PlateCarree())
+        ax.set_yticks(yticks, crs=ccrs.PlateCarree())
+        lon_formatter = LongitudeFormatter(zero_direction_label=True)
+        lat_formatter = LatitudeFormatter()
+        ax.xaxis.set_major_formatter(lon_formatter)
+        ax.yaxis.set_major_formatter(lat_formatter)
 
-    img=ax.scatter(gauge_information["longitude"],
-                   gauge_information["latitude"],
-                   transform=ccrs.PlateCarree(),
-                   c=gauge_information["gauge id"],
-                   s=2,
-                   cmap="plasma")
-    ax.set_title("Geographic locations of gauges in CAMELS dataset")
-    asp = ax.get_aspect()
+        img=ax.scatter(gauge_information["longitude"],
+                    gauge_information["latitude"],
+                    transform=ccrs.PlateCarree(),
+                    c=gauge_information["gauge id"],
+                    s=2,
+                    cmap="plasma")
+        ax.set_title("Geographic locations of gauges in CAMELS dataset")
+        asp = ax.get_aspect()
 
-    ax = plt.subplot(gs[1])
-    cb = plt.colorbar(img, label="Gauge ID", cax=ax)
-    cb.formatter.set_useOffset(False)
-    cb.formatter.set_scientific(False)
-    cb.formatter.set_powerlimits((-10, 20))
-    cb.update_ticks()
-    plt.show()
+        ax = plt.subplot(gs[1])
+        cb = plt.colorbar(img, label="Gauge ID", cax=ax)
+        cb.formatter.set_useOffset(False)
+        cb.formatter.set_scientific(False)
+        cb.formatter.set_powerlimits((-10, 20))
+        cb.update_ticks()
+        plt.tight_layout()
+        plt.show()
+
+    except:
+        url = "http://spfrnd.de/datasets/camels/overview.png"
+        img = plt.imread(url)
+        plt.imshow(img)
+        plt.gca().set_axis_off()
+        plt.show()
 
 def plot_basin(gauge_id,
                dlat=0.05,
@@ -68,10 +74,6 @@ def plot_basin(gauge_id,
         tile_level: The resolution level to use for the terrain
             in the background.
     """
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from matplotlib.gridspec import GridSpec
-
     if not gauge_id in gauge_ids:
         other = gauge_ids[np.argmin(np.abs(int(gauge_id) - gauge_ids))]
         raise ValueError("Gauge ID {} is not available from this dataset. The "
@@ -123,10 +125,7 @@ def plot_basin(gauge_id,
         plt.show()
     except:
         url = "http://spfrnd.de/datasets/camels/plots/{}.png".format(gauge_id)
-        with request.urlopen(url) as response:
-            with NamedTemporaryFile() as f:
-                shutil.copyfileobj(response, f)
-                img = plt.imread(f.name)
-                plt.imshow(img)
-                plt.gca().set_axis_off()
+        img = plt.imread(url)
+        plt.imshow(img)
+        plt.gca().set_axis_off()
         plt.show()
